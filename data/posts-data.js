05c265de2db38e2894319a521697e26e5ac01a3e
lanletter <lanletter@qq.com>
2017-04-24 22:04:42 +0800

    音乐播放初步实现

--- data/posts-data.js
+++ data/posts-data.js
@@ -2,34 +2,66 @@ var local_database = [
     {
         imgCat: "/images/cats/1.png",
         tittle: "呼朋引伴",
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "呼朋引伴",
+        }
     },
     {
         imgCat: "/images/cats/2.png",
         tittle: "呼噜呼噜",
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "呼噜呼噜",
+        }
     },
     {
         imgCat: "/images/cats/3.png",
         tittle: "幼猫",
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "幼猫",
+        }
     },
     {
         imgCat: "/images/cats/4.png",
         tittle: "老猫",
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "老猫",
+        }
     },
     {
         imgCat: "/images/cats/5.png",
         tittle: "耍赖",
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "耍赖",
+        }
     },
     {
         imgCat: "/images/cats/6.png",
         tittle: "撒娇",
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "撒娇",
+        }
     },
     {
         imgCat: "/images/cats/7.png",
         tittle: "攻击",
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "攻击",
+        }
     },
     {
         imgCat: "/images/cats/8.png",
         tittle: "逃走",
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "逃走",
+        }
     }
 ]
 
--- pages/cats/cats.js
+++ pages/cats/cats.js
@@ -1,13 +1,33 @@
 //必须用相对路径
-var postsData=require('../../data/posts-data.js')
+var postsData = require('../../data/posts-data.js')
 
 Page({
-    data:{
+    data: {
         //小程序总是会读取data对象来做数据绑定，这个动作我们称为动作A
         //而这个动作A的执行，是在onLoad事件执行之后发生的
+        
     },
-    onLoad:function(options){
+
+    onLoad: function (options) {
         //页面初始化，options为页面跳转所带来的参数
-        this.setData({postList_key:postsData.postList});
-    }
+        this.setData({ postList_key: postsData.postList });
+    },
+
+    onMusicTap: function (event) {
+        var isPlayingMusic=this.data.isPlayingMusic;
+        if(isPlayingMusic){
+            wx.stopBackgroundAudio();
+            this.setData({
+                isPlayingMusic:false
+            })
+        }else{
+            wx.playBackgroundAudio({
+              dataUrl: 'http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38',
+              title:"张国荣",
+            })
+            this.setData({
+                isPlayingMusic:true
+            })
+        }
+    },
 })
